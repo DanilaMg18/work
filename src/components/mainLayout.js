23d49087efcd2bcfe5c4abf0b7b35bd243debb4b
Danila Palcech <dpalcech@gmail.com>
2023-01-24 18:15:32 +0200

    slop

--- src/components/mainLayout.js
+++ src/components/mainLayout.js
@@ -1,30 +1,59 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 
 function Main() {
   return (
     <div>
       <header>
         <div className='header_lefside'>
-          <img src="./photos/Logo.png" alt='foto'/>
+          <img className='logo' src="./assets/photos/Logo.png" alt='foto'/>
           <nav>
-            <Link className='LinkNav'>Каталог</Link>
-            <Link className='LinkNav'>О проекте</Link>
-            <Link className='LinkNav'>Птицы</Link>
-            <Link className='LinkNav'>Пожертвования</Link>
+            <Link className='LinkNav1'>Каталог</Link>
+            <img className='navvector' src='./assets/photos/catalog vector.svg'/>
+            <Link className='LinkNav2'>О проекте</Link>
+            <Link className='LinkNav3'>Птицы</Link>
+            <Link className='LinkNav4'>Пожертвования</Link>
           </nav>
         </div>
         <div className='header_rightside'>
             <div className='header_rightside_up'>
-              <img src='./photos/phone vector.svg'/>
-              <p>8 920 999 43 50</p>
+              <img src='./assets/photos/phone vector.svg'/>
+              <p className='telnumber'>8 920 999 43 50</p>
+              <img className='vectorheaderleftside' src='./assets/photos/Buy.svg'/>
+              <img src='./assets/photos/account.svg'/>
+            </div>
+            <div className='header_rightside_down'>
+              <input className='inputheader' placeholder='Поиск'/>
+              <img className='vectorheaderrightside' src='/assets/photos/Search.svg'/>
             </div>
         </div>
       </header>
       <main>
-        
+        <Outlet/>
       </main>
-      <footer></footer>
+
+      <div className='likefooter'>
+        <div className='likefooter_leftside'>
+          <h1 className='donate'>Пожертвуйте на благо проекта</h1>
+          <div className='checkboxandtexts'>
+            <input className='checkboxfooter' type='checkbox'/>
+            <p className='pcheckbox'>Единоразовая выплата</p>
+            <input className='checkboxfooter' type='checkbox'/>
+            <p className='pcheckbox'>Ежемесячные выплаты</p>
+          </div>
+          <div className='inputandbutton'>
+            <input className='inputfooter' placeholder='Введите сумму'/>
+            <button className='buttonfooter'>Пожертвовать</button>
+          </div>
+        </div>
+        <div className='likefooter_rightside'>
+
+        </div>
+      </div>
+
+      <footer>
+
+      </footer>
     </div>
   );
 }
